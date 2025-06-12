63ee5a3daaeb0c896e24cabcec67aee79978dbaf
3uomlkh <3uomlkh@gmail.com>
2025-06-12 16:58:12 +0900

    fix(test): BackendApplicationTests 수정 #7

--- src/test/java/com/example/backend/BackendApplicationTests.java
+++ src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "jwt.secret.key=YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWE="
+})
 class BackendApplicationTests {
 
     @Test
