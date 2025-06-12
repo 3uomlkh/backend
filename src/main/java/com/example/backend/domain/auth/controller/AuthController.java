55904a13d48bd5ad6edc5d6a584c61e0943d9eac
3uomlkh <3uomlkh@gmail.com>
2025-06-12 11:21:00 +0900

    docs(auth): 인증/인가 API에 Swagger 어노테이션 적용 #6

--- src/main/java/com/example/backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/backend/domain/auth/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.example.backend.domain.auth.dto.request.SignupRequest;
 import com.example.backend.domain.auth.dto.response.SigninResponse;
 import com.example.backend.domain.auth.dto.response.SignupResponse;
 import com.example.backend.domain.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증/인가 API")
+@SecurityRequirement(name = "bearer-key")
 @RequestMapping("/api/auth")
 @RestController
 @RequiredArgsConstructor
@@ -20,11 +25,13 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "회원가입")
     @PostMapping("/signup")
     public ResponseEntity<SignupResponse> signup (@Valid @RequestBody SignupRequest request) {
         return ResponseEntity.ok(authService.signup(request));
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/signin")
     public ResponseEntity<SigninResponse> signin (@RequestBody SigninRequest request) {
         return ResponseEntity.ok(authService.signin(request));
