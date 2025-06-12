eb562c393402186dbcaf2f8eaae004a6a94583ae
3uomlkh <3uomlkh@gmail.com>
2025-06-12 11:21:28 +0900

    docs(user): 유저 API에 Swagger 어노테이션 적용 #6

--- src/main/java/com/example/backend/domain/user/controller/UserController.java
+++ src/main/java/com/example/backend/domain/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.backend.domain.user.controller;
 import com.example.backend.domain.user.dto.response.GrantAdminResponse;
 import com.example.backend.domain.user.enums.UserRole;
 import com.example.backend.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "유저 API")
 @RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +21,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "관리자 권한 부여")
     @PatchMapping("/admin/users/{userId}/roles")
     @Secured(UserRole.Authority.ADMIN)
     public ResponseEntity<GrantAdminResponse> grantAdmin(
